34fbde8e46c68935bf0ed7fd41a93629e93ea968
ngeyen <ngeyennv@hotmail.com>
2020-07-29 23:17:47 +0200

    styled blog images renamed the project updated urls updated database pictures configured local cdn server

--- djangoBlog/src/djangoBlog/wsgi.py
+++ djangoBlog/src/djangoBlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for trydjango project.
+WSGI config for djangoBlog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trydjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBlog.settings')
 
 application = get_wsgi_application()
